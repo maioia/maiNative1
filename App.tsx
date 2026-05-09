e96a2a662d21f34aec4b68e73d321500f0817229
Mai.A.K <mai_e_engineer@yahoo.com>
2026-05-10 01:11:50 +0200

    know main components in react native

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titleText}>hello!</Text>
+      <Image
+        source={require("./assets/splash-icon.png")}
+        style={{ width: 200, height: 200 }}
+      />
+      <Image
+        source={{
+          uri: "https://www.clariontech.com/hubfs/MicrosoftTeams-image%20(13)-1.png",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
     </View>
   );
 }
@@ -13,8 +21,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
+
+  titleText: { textAlign: "center", fontSize: 100, color: "#000" },
 });
