a146e30e028461da576220739a2e60682b6c386b
Mai.A.K <mai_e_engineer@yahoo.com>
2026-05-16 03:19:50 +0200

    add styling in react native practice

--- App.tsx
+++ App.tsx
@@ -1,28 +1,108 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import {
+  Alert,
+  Button,
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  TouchableOpacity,
+  ScrollView,
+  Platform,
+  ActivityIndicator,
+} from "react-native";
 
 export default function App() {
+  const onButtonPress = () => {
+    Alert.alert("Pressed");
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.titleText}>hello!</Text>
-      <Image
-        source={require("./assets/splash-icon.png")}
-        style={{ width: 200, height: 200 }}
-      />
-      <Image
-        source={{
-          uri: "https://www.clariontech.com/hubfs/MicrosoftTeams-image%20(13)-1.png",
-        }}
-        style={{ width: 200, height: 200 }}
-      />
-    </View>
+    <>
+      <View style={styles.container}>
+        //{" "}
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ backgroundColor: "green" }}
+        >
+          <Button title="press me" onPress={onButtonPress} />
+          <Text style={styles.titleText}>hello!</Text>
+          <Image
+            source={require("./assets/splash-icon.png")}
+            style={{ width: 200, height: 200 }}
+          />
+          <Pressable onPress={onButtonPress}>
+            <Image
+              source={{
+                uri: "https://www.clariontech.com/hubfs/MicrosoftTeams-image%20(13)-1.png",
+              }}
+              style={{ width: 200, height: 200 }}
+            />
+          </Pressable>
+          <TouchableOpacity>
+            <Image
+              source={{
+                uri: "https://www.clariontech.com/hubfs/MicrosoftTeams-image%20(13)-1.png",
+              }}
+              style={{ width: 200, height: 200 }}
+            />
+          </TouchableOpacity>
+          <Image
+            source={{
+              uri: "https://www.clariontech.com/hubfs/MicrosoftTeams-image%20(13)-1.png",
+            }}
+            style={{ width: 200, height: 200 }}
+          />{" "}
+          <Image
+            source={{
+              uri: "https://www.clariontech.com/hubfs/MicrosoftTeams-image%20(13)-1.png",
+            }}
+            style={{ width: 200, height: 200 }}
+          />{" "}
+          <Image
+            source={{
+              uri: "https://www.clariontech.com/hubfs/MicrosoftTeams-image%20(13)-1.png",
+            }}
+            style={{ width: 200, height: 200 }}
+          />
+        </ScrollView>
+      </View>
+
+      <View style={styles.view2}></View>
+
+      <View style={styles.container}>
+        <Text
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          my {Platform.OS === "android" ? "Android" : "IOS"} code here{" "}
+        </Text>
+        <ActivityIndicator size="large" color="red" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    width: 200,
+    height: 100,
     flex: 1,
-    backgroundColor: "#fff",
+    flexDirection:"column-reverse",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
   },
 
   titleText: { textAlign: "center", fontSize: 100, color: "#000" },
+
+  view2: {
+    flex:1,
+    width: 300,
+    height: 100,
+    flexDirection:"row-reverse",
+    backgroundColor: "blue",
+    marginVertical:20
+  },
 });
